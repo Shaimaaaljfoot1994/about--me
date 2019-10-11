1f08947fc923844c7763e902c930fe1076e00672
shaimaa <Shaimaa.aljfoot@gmil.com>
2019-10-11 17:19:47 +0300

    new

--- js/js.js
+++ js/js.js
@@ -1,108 +1,162 @@
-'use strict';
-var name = prompt(' what is your name?');
-while ( !isNaN(name)){
-name =prompt('please ,write your name first!\n\nAnd you allow to enter only text .')
+/*"use strict";
+var username=prompt("what is your name?");
+console.log ( "username", username )
+alert ("welcom"+ username);
+var age= prompt ("are you 25 years old ?");
+console.log ( "age", age )
+ age =age.toLowerCase();
+if (age=== "yes" || age === "y"){
+    alert ("it's my age" );
+} else if (age==="no"){
+    alert ( "maybe your older or younger than me");
+}
+var name = prompt ( " do you know that  family of shaimaa is jfoot? ?");
+console.log ( "name", name )
+name =name.toLowerCase();
+if ( name=== "yes" || name=== "y"){
+    alert ( "it's my name famiy")
+}else if ( name==="no"){
+    alert ( "new information to know and it's my name family");
+}
+var food= prompt (" do love food? ?");
+console.log ( "food", food )
+food =food.toLowerCase();
+if ( food=== "yes" || food=== "y"){
+    alert("me too");
+}else if ( food==="no"){
+    alert ("unfortunalety");
+}
+var read= prompt (" do you like reading a lot? ?");
+console.log ( "read",read )
+read =read.toLowerCase();
+if ( read=== "yes" || read=== "y"){
+    alert ("me too ");
+}else if ( read==="no"){
+    alert ("reading will make you better i'm sure");
+}
+var game=prompt ("do you like PUBG game ");
+console.log ( "game",game )
+game =game.toLowerCase();
+if ( game=== "yes" || game=== "y"){
+    alert("me too");
+}else if ( game==="no"){
+    alert ("try it, you will fell excited");
+} /** */
+"use Strict";
+username = prompt("what is your name?");
+alert("welcom" + username);
+console.log("username" + username);
+var count=0;
+// var questions = [
+//     [
+//         "are you 25 years old ?",
+//         "it's my age",
+//         "maybe your older or younger than me",
+//     ],
+//     [
+//         " do you know that  family of shaimaa is jfoot?  ?",
+//         "it's my name family",
+//         "new information to know and it's my name family",
+//     ],
+//     [
+//         "   do love food?",
+//         "me too",
+//         "unfortunalety",
+//     ],
+//     [
+//         " do you like reading a lot? ",
+//         "me too ",
+//         "i think you must start reading",
+//     ],
+//     [
+//         "do you like PUBG game ",
+//         "me too",
+//         "try it, you will fell excited",
+//     ]
+// ]
+// for (var i = 0; i < 5; i++) {
+//     var answer = questions[i];
+//     var promptQuestion = answer[0];
+//     var posetiveReply = answer[1];
+//     var negativeReply = answer[2];
+//     var username = prompt(promptQuestion);
+//     promptQuestion = promptQuestion.toLowerCase();
+
+//     switch (username){
+//         case 'yes':
+//         case  'y':
+//              alert(posetiveReply);
+//         var count= count+1
+//              break;
+//         case 'no':
+//         case 'n':
+//             alert(negativeReply);
+//             break;
+//     default :
+//     alert("incorret answer")
+//     }
+//  }
+function userinput1 ( question, yes , no ) {
+var response = prompt (question);
+response = response.toLowerCase();
+
+if (response == 'yes' || response =='y'){
+    alert(yes);
+    count = count +1;
+
+} else if (response == 'no' || response == 'n'){
+alert(no);    
+}
 }
 
-alert('welcom  ' + name + '  welcome to my bage ');
-console.log('name', name);
+userinput1('are you 25 years old ?', "it's my age", "maybe your older or younger than me");
+userinput1(" do you know that  family of shaimaa is jfoot?  ?", "it's my name family", "new information to know and it's my name family");
+userinput1(   " do love food? ?", "me too","unfortunalety");
+userinput1(  " do you like reading a lot? ?", "me too ", "i think you must start reading");
+userinput1("do you like PUBG game ","me too", "try it, you will fell excited");
 
-var shaimaa = [
-    ['Is the last name of \"Shaimaa\" is \"jfoot\"?',
-    'Great answer',
-    'Oooh, wrong annswer'
-    ],
-    ['Is shaimaa age 25 years?',
-    'Great one, keep going',
-    'Wrong answer, please enter \"yes\" OR \"no\"'
-    ],
-    ['do Shaimaa love swimming ?',
-    'Of course i do',
-    'Sorry about that, wrong answer'
-    ],
-    ['Has shaimaa studied at the ttu?',
-    'Wow, Great answer',
-    'thats rong'
-    ],
-    ['do you think shaimaa loves basketball?',
-    'Oooh, wrong annswer, sorry about that',
-    'of course i dont'
-    ]
-]
+///////////////////////////////////////////Q6//////////////////////////////////////
+function age () {
 
-
-
-getQuestion();
-
-function getQuestion(){
-    for (var i = 0; i < shaimaa.length; i++) {
-        var shaimaa2 = shaimaa[i];
-        var userreply = prompt(shaimaa2[0]);
-        userreply = userreply.toLowerCase();
-        var yesreply = shaimaa2[1];
-        var noreply = shaimaa2[2];
-        switch (userreply) {
-            case 'yes':
-            case 'y':
-                alert(yesreply);
-                break;
-            case 'no':
-            case 'n':
-                alert(noreply);
-                break;
-            default:
-                alert('wrong answer');
-        }
-    }
-}
-
-var score = 0, age, num1;
-
-for (var j = 0; j <= 4; j++) {
-    age = prompt('do you know how old iam? ');
-    num1 = parseInt(age);
-    if(num1 == 25){
-        alert('correct answer  iam ' + num1 + ' years old ');
-        score++;
-        j=5;
-        break;
-    }else if (num1 < 18) {
-        alert('your answer is too low.');
-    }else if (num1 > 30) {
-        alert('your answer is too high');
-    }else if(isNaN(num1)){
-        alert('please, you allow to enter only number');
-    }
-}
-
-
-var color = ['red', 'black', 'yallow', 'blue', 'green', 'White'];
-var userreplies = prompt('what is the color that i like ?');
-userreplies = userreplies.toLowerCase();
-for (var h = 0; h < color.length; h++){
-switch (userreplies) {
-    case 'red':
-    case 'black':
-    case 'blue':
-    case 'green':
-    case 'Yellow':
-    case 'White':
-        alert('correct answer ' );
-        var userreplies = prompt('what is the  other color that i like ?');
-        score++;
-        break;
-    default:
+    for (var i = 0; i < 4; i++) {
+        var number = prompt("How many my age ?");
+        if (number > 25) {
+        alert("too high, please try again");
         
-            alert('wrong answer');
-            var userreplies = prompt('please reanswer the question?and choose from these choice (purple , blue , white, black, green, broun, red , orang ,grey)');
-
-        }
+    } else if (number < 25) {
+        alert("too low, please try again");
+        
+    } else if (number == 25){
+        alert("that's correct");
+        count=count+1
+        break;
+    } 
+    if (i==3){
+     alert("the correct answer = 25");
 }
-alert('your score is '+ score)
-alert('the color that i like  '+ color[0] + ' ,' + color[1] + ', ' + color[2] + ', ' + color[3] + ' ,' + color[4] + ' and ' + color[5]);
-alert('Bye bye   ' + name + ', I hope that you enjoy visiting my site ');
-console.log('name', name);
-console.log('userreplay', userreply);
-console.log('age', age);
-console.log('userreplies)', userreplies);
-// document.write('<h2>'+score+'</h2>')
+}
+return(number);
+}
+age ();
+
+
+
+// ////////////////////////Q7////////////////////////////////////
+
+function color () {
+var lovecolor = ["yallow", "White", "black", "green", "blue", "red"]
+for (i = 0; i < 6; i++) {
+    var user = prompt("guess one of my top ten lovecolor")
+    if (user === lovecolor[0] || user == lovecolor[1] || user === lovecolor[2] || user === lovecolor[3] || user === lovecolor[4] || user === lovecolor[5]) {
+        alert("correct answer");
+        count=count+1
+        break;
+    } else {
+        alert("you have to try a gain");
+    }
+}
+alert('your correct answer= '+ count);
+
+ return (lovecolor);   
+}
+color();
