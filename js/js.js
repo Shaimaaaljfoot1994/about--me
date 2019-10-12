ff9dd56cecc6a69340d1c2584afadf374d51a610
shaimaa <Shaimaa.aljfoot@gmil.com>
2019-10-12 17:01:06 +0300

    new

--- js/js.js
+++ js/js.js
@@ -1,48 +1,4 @@
 
-/*"use strict";
-var username=prompt("what is your name?");
-console.log ( "username", username )
-alert ("welcom"+ username);
-var age= prompt ("are you 25 years old ?");
-console.log ( "age", age )
- age =age.toLowerCase();
-if (age=== "yes" || age === "y"){
-    alert ("it's my age" );
-} else if (age==="no"){
-    alert ( "maybe your older or younger than me");
-}
-var name = prompt ( "do you know that  family of shaimaa is jfoot?");
-console.log ( "name", name )
-name =name.toLowerCase();
-if ( name=== "yes" || name=== "y"){
-    alert ( "it's my name family")
-}else if ( name==="no"){
-    alert ( "new information to know and it's my name family");
-}
-var food= prompt (" do love food?");
-console.log ( "food", food )
-food =food.toLowerCase();
-if ( food=== "yes" || food=== "y"){
-    alert("me too");
-}else if ( food==="no"){
-    alert ("unfortunalety");
-}
-var read= prompt (" do you like reading a lot? ?");
-console.log ( "read",read )
-read =read.toLowerCase();
-if ( read=== "yes" || read=== "y"){
-    alert ("me too ");
-}else if ( read==="no"){
-    alert ( "i think you must start reading");
-}
-var game=prompt ("do you like PUBG game ");
-console.log ( "game",game )
-game =game.toLowerCase();
-if ( game=== "yes" || game=== "y"){
-    alert("me too");
-}else if ( game==="no"){
-    alert ("try it, you will fell excited");
-} /** */
 "use Strict";
 username = prompt("what is your name?");
 alert("welcom" + username);
@@ -110,11 +66,12 @@ function userinput1 ( question, yes , no ) {
     }
     }
     
-    userinput1('are you 25 years old ?', "it's my age", "maybe your older or younger than me");
-    userinput1(" do you know that  family of shaimaa is jfoot?  ?", "it's my name family", "new information to know and it's my name family");
-    userinput1(   " do love food? ?", "me too","unfortunalety");
-    userinput1(  " do you like reading a lot? ?", "me too ", "i think you must start reading");
-    userinput1("do you like PUBG game ","me too", "try it, you will fell excited");
+    // userinput1('are you 25 years old ?', "it's my age", "maybe your older or younger than me");
+    // userinput1(" do you know that  family of shaimaa is jfoot?  ?", "it's my name family", "new information to know and it's my name family");
+    // userinput1(   " do love food? ?", "me too","unfortunalety");
+    // userinput1(  " do you like reading a lot? ?", "me too ", "i think you must start reading");
+    // userinput1("do you like PUBG game ","me too", "try it, you will fell excited");
+
 
 ///////////////////////////////////////////Q6//////////////////////////////////////
 function age () {
@@ -154,7 +111,6 @@ function color () {
         } else {
             alert("you have to try a gain");
             
-
         }
         
     }
